2ddce9d649d6218e1c8a19bd27be32421eec6ca6
katerz avocado <ktrinaapodaca@yahoo.com>
2021-09-28 01:49:32 -0600

    adding index.js

---
+++ server.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose");
+const logger = require("morgan");
+const express = require("express");
+
+const PORT = process.env.PORT || 3000;
+
+const db = require("./models");
+
+const app = express();
+
+app.use(logger("dev"));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static("public"));
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/tracker", {
+  useNewUrlParser: true,
+});
