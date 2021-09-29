430e1bc37319d2344a3cd4c05824a4efad56e3dd
katerz avocado <ktrinaapodaca@yahoo.com>
2021-09-29 03:07:12 -0600

    updating server.js

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose");
 const logger = require("morgan");
 const express = require("express");
+const path = require("path");
 
 const PORT = process.env.PORT || 3000;
 
@@ -25,13 +26,30 @@ app.get("/stats", (req, res) => {
 });
 
 app.get("/exercise", (req, res) => {
-  res.sendFile(path.join(__dirname, "/public/index.htnml"));
+  res.sendFile(path.join(__dirname, "/public/index.html"));
 });
 
 app.get("/exercise", (req, res) => {
   res.sendFile(path.join(__dirname, "/public/exercise.html"));
 });
 
+// caddExercise , getLastWorkout , createWorkout , getWorkoutsInRange
+app.get("/api/workouts/:id", (req, res) => {
+  db.workouts.find({});
+  // call function to updated created workout??
+});
+
+app.get("/api/workouts", (req, res) => {
+  db.workouts.find({});
+  // create workout and addExercise? ?
+});
+
+app.get("/api/workouts/range", (req, res) => {
+  db.workouts.find({});
+  // call function to get getWorkoutRange???
+});
+
+// i don't think i need this?
 db.workouts
   .create({ name: "Workout Tracker" })
   .then((dbworkouts) => {
